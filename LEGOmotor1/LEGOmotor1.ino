b16ac9993765b928a54a4d32f87b5d0cf418c87b
MormelCat <arjan.de.jong@me.com>
2016-08-07 12:31:12 +0200

    test run dc motor

---
+++ LEGOmotor1/LEGOmotor1.ino
@@ -0,0 +1,60 @@
+
+const unsigned int controlPin1A = 2;
+const unsigned int controlPin2A = 5;
+const unsigned int ENablePin = 9;
+unsigned int motorSpeed = 0;
+unsigned int motorDirection = 1;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+pinMode(controlPin1A, OUTPUT);
+pinMode(controlPin2A, OUTPUT);
+pinMode(ENablePin, OUTPUT);
+digitalWrite(ENablePin, LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+
+
+analogWrite(ENablePin, 100);
+
+digitalWrite(controlPin1A, HIGH);
+digitalWrite(controlPin2A, LOW);
+
+
+delay(5000);
+
+digitalWrite(controlPin1A, LOW);
+digitalWrite(controlPin2A, HIGH);
+
+delay(5000);
+
+
+
+/*
+for (int i = 0; i <256; i++)
+  {
+analogWrite(ENablePin, i);
+digitalWrite(controlPin1A, HIGH);
+digitalWrite(controlPin2A, LOW);
+delay(100);
+  }
+*/
+
+
+
+
+
+
+
+
+
+
+
+}
